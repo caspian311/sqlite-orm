3c813c82a260d967fc1fd4aec0bd1e469c8afe2d
matt.todd <matt.c.todd@gmail.com>
2011-01-20 21:41:57 -0600

    updated build process to do it all\!

--- build.sh
+++ build.sh
@@ -1,10 +1,48 @@
 #!/bin/bash
 
+ANDROID_SDK_PATH=~/.apps/android-sdk-linux_86/tools
+
+echo Cleaning out the old build
+rm -f *.jar *.apk
+
 pushd ./sqlite-orm
-
-mvn clean package
-cp target/sqlite-orm*.jar ..
-
+mvn clean
 popd
 
- adb shell am instrument -w net.todd.sqliteorm/android.test.InstrumentationTestRunner
+pushd sqlite-orm-androidtest
+ant clean
+popd
+
+echo Building the sqlite-orm-???.jar file
+pushd ./sqlite-orm
+mvn package
+cp target/sqlite-orm*.jar ../sqlite-orm-androidtest/libs
+popd
+
+echo Building test application
+pushd ./sqlite-orm-androidtest
+ant debug
+cp bin/*.apk ..
+popd
+
+device=`$ANDROID_SDK_PATH/android list avd | grep Name | awk '{print $2}' | head -1`
+
+echo Starting emulator: $device
+$ANDROID_SDK_PATH/emulator -noskin -avd $device &
+emulator_process_id=$!
+echo Emulator process id: $emulator_process_id
+
+echo Waiting for device to start...
+$ANDROID_SDK_PATH/adb wait-for-device
+
+echo Installing test application on device
+$ANDROID_SDK_PATH/adb install sqlite-orm-test-debug.apk
+
+echo Device is up, running tests
+$ANDROID_SDK_PATH/adb shell am instrument -w net.todd.sqliteorm/android.test.InstrumentationTestRunner
+
+echo Removing test application from device
+$ANDROID_SDK_PATH/adb uninstall net.todd.sqliteorm
+
+echo Killing off emulator...
+kill $emulator_process_id
